707e6ad67f19eec329f2c0d105d9216d350b3ac2
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-08 01:49:14 +0100

    Create PensionServiceApplication.java

---
+++ src/main/java/com/shotaroi/pensionservice/PensionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shotaroi.pensionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensionServiceApplication.class, args);
+    }
+}
